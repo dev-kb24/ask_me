e23af0233fe39c138246928c5a1121d723416e6b
rémi <dufour.remi59@gmail.com>
2023-01-19 23:03:24 +0100

    opti client

--- ask_me_client/src/App.vue
+++ ask_me_client/src/App.vue
@@ -1,8 +1,22 @@
-<script setup>
-
+<script>
+import axios from 'axios';
+export default {
+  data(){
+    return {
+        phrases:[]
+    }
+  },
+ async created(){
+    await axios.get('http://localhost:3000/api/phrases').then((res)=>{
+      this.phrases = res.data;
+    })
+  }
+}
 </script>
-
-<template>
  
+<template>
+  <ul v-for="p in phrases" :key="p">
+    <li>{{ p.phrase }}</li>
+  </ul>
 </template>
 
