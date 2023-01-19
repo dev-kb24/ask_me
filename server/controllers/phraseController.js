233e93181f20a360e6e323431041ddddd358d357
rémi <dufour.remi59@gmail.com>
2023-01-19 23:03:57 +0100

    petites modifs

--- server/controllers/phraseController.js
+++ server/controllers/phraseController.js
@@ -1,7 +1,7 @@
 export const phraseController = (Phrase) => {
     return async (req, res,next) => {
         try {
-            const phrases = await Phrase.find();
+            const phrases = await Phrase.find()
             res.json(phrases);
         } catch (err) {
             console.log(err);
--- server/index.js
+++ server/index.js
@@ -1,6 +1,9 @@
 import express from 'express'
 import mongoose from 'mongoose'
 import router from './router.js'
+import cors from 'cors';
+import bodyParser from 'body-parser';
+
 const app = express()
 const port = 3000
 
@@ -9,6 +12,18 @@ mongoose.connect('mongodb://localhost:27017/askme', { useNewUrlParser: true, use
     .then(() => console.log('Connected to MongoDB...'))
     .catch(err => console.error('Could not connect to MongoDB...', err))
 
+app.use(cors({
+      origin: '*',
+    
+      methods: [
+        'GET',
+        'POST',
+      ],
+    
+      allowedHeaders: [
+        'Content-Type',
+      ],
+    }))
 app.use('/api', router)
 
 app.listen(port, () => {
