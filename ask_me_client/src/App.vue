32377546976e67670884c8bc5a606bb5b84c8cb2
rémi <dufour.remi59@gmail.com>
2023-01-22 22:17:22 +0100

    ajout add et delete phrase

--- ask_me_client/src/App.vue
+++ ask_me_client/src/App.vue
@@ -3,20 +3,59 @@ import axios from 'axios';
 export default {
   data(){
     return {
-        phrases:[]
+        phrases:[],
+        phrase:null
     }
   },
  async created(){
     await axios.get('http://localhost:3000/api/phrases').then((res)=>{
-      this.phrases = res.data;
+      if(res.data.success){
+        this.phrases = res.data.data;
+      }
     })
+  }, 
+  methods:{
+   async handleSubmit()
+    {
+      if(this.phrase !== null){
+        let data = {
+          phrase:this.phrase
+        }
+
+        await axios.post('http://localhost:3000/api/phrases/add',data,{
+          headers: {
+            'Content-Type': 'application/json'
+          }
+        }).then((res)=>{
+            if(res.data.success){
+              this.phrases.push(res.data.data);
+            }
+        }).finally(() => {
+          this.phrase = null;
+        })
+      }
+    },
+    async deleteById(id)
+    {
+      await axios.delete(`http://localhost:3000/api/phrases/delete/${id}`).then( res => {
+          if(res.data.success){
+            this.phrases = this.phrases.filter(phrase => phrase._id !== res.data.id);
+          }
+        })
+    }
   }
 }
 </script>
  
 <template>
-  <ul v-for="p in phrases" :key="p">
-    <li>{{ p.phrase }}</li>
-  </ul>
+  <div>
+    <ul v-for="p in phrases" :key="p">
+      <li>{{ p.phrase }} <button @click="deleteById(p._id)">delete</button></li>
+    </ul>
+    <form @submit.prevent="handleSubmit">
+      <input type="text" v-model="phrase" placeholder="Name">
+      <button type="submit">Envoyer</button>
+    </form>        
+  </div>
 </template>
 
